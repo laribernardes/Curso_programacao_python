e04ced2889fb4476ce3e8fe662f78361f2b8bafd
Lari Bernardes <bernardesl171@gmail.com>
2026-01-28 20:59:01 -0300

    Delete Aula05 directory

--- Aula05/exercício01.py
+++
@@ -1,9 +0,0 @@
-idade = int(input('Digite sua idade: '));
-senha = int(input('Digite sua senha: '));
-
-if (idade >= 18) and (senha == 1234):
-    print('Acesso permitido')
-
-else:
-    print('Acesso negado')
-
